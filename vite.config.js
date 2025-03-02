c36c3d556e744962fe0b18c8ba9329bd492675fc
kundanrkr <kundan08nwd@gmail.com>
2025-03-02 19:26:08 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_GITHUB_PAGES === "true" ? "/your-repo-name/" : "/",
+  base: process.env.VITE_GITHUB_PAGES === "true" ? "/portfolio/" : "/",
 });
